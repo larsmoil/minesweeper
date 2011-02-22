a94c3d8af75fc73b05bc5d908d6d9253e947539b
larsmoil <lars.olof.moilanen@gmail.com>
2011-02-22 20:51:40 +0100

    One and two dimensions

--- spec/minesweeper_spec.rb
+++ spec/minesweeper_spec.rb
@@ -3,21 +3,25 @@ require 'spec_helper'
 class Minesweeper
 
   def board(specification)
-    @cells = specification.split
-    result = []
+    res = []
+    specification.each do |row|
+      @cells = row.split
+      result = []
 
-    @cells.each_with_index do |c, i|
-      if mine?(i)
-        result << "x"
-      elsif next_cell_is_mine?(i) && previous_cell_is_mine?(i)
-        result << "2"
-      elsif next_cell_is_mine?(i) || previous_cell_is_mine?(i)
-        result << "1"
-      else
-        result << c
+      @cells.each_with_index do |c, i|
+        if mine?(i)
+          result << "x"
+        elsif next_cell_is_mine?(i) && previous_cell_is_mine?(i)
+          result << "2"
+        elsif next_cell_is_mine?(i) || previous_cell_is_mine?(i)
+          result << "1"
+        else
+          result << c
+        end
       end
+      res << result.join(" ")
     end
-    result.join(" ")
+    res
   end
 
   def mine?(i)
@@ -34,36 +38,23 @@ end
 
 describe Minesweeper do
   context "Contains 1 row with" do
-    context "no mines" do
-      it "returns all blank cells" do
-        subject.board("_ _ _").should == "_ _ _"
+    it "works in 1 dimension" do
+      {
+        ["_ _ _"] => ["_ _ _"],
+        ["x _ _"] => ["x 1 _"],
+        ["x _ x"] => ["x 2 x"],
+        ["x x _"] => ["x x 1"],
+        ["_ _ x _"] => ["_ 1 x 1"],
+        }.map do |k,v|
+          subject.board(k).should == v
+        end
       end
     end
-    context "one mine" do
-      it "shows 1 next to the mine" do
-        subject.board("x _ _").should == "x 1 _"
-      end
-      it "shows 1 x 1" do
-        subject.board("_ x _").should == "1 x 1"
-      end
-      it "shows _ 1 x 1" do
-        subject.board("_ _ x _").should == "_ 1 x 1"
-      end
-    end
-    context "two mines" do
-      it "shows x 2 x" do
-        subject.board("x _ x").should == "x 2 x"
-      end
-      it "shows x x 1" do
-        subject.board("x x _").should == "x x 1"
+    context "Contains 2 row with" do
+      context "no mines" do
+        it "returns all blank cells" do
+          subject.board(["_ _ _", "_ _ _"]).should == ["_ _ _", "_ _ _"]
+        end
       end
     end
   end
-  context "Contains 2 row with" do
-    context "no mines" do
-      it "returns all blank cells" do
-        subject.board(["_ _ _", "_ _ _"]).should == ["_ _ _", "_ _ _"]
-      end
-    end
-  end
-end
